fb78722f3f907698d48ad71652d8e2206b015526
mtyszko <michal.tyszko@gmail.com>
2020-09-22 08:47:18 +0200

    adding metric units and lang pl

--- utils/get_forecast.js
+++ utils/get_forecast.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const getForecast = async (lat, lon) => {
   try {
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-exclude={part}&appid=${process.env.OPENWEATHER_API_KEY}`;
+exclude={part}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric&lang=pl`;
 
     const serverRes = await fetch(url);
     const data = await serverRes.json();
